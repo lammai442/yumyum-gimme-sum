f861cf853d9349386e49b5d04b4f38570db8b3c4
lammai442 <lam.mai442@gmail.com>
2025-03-28 16:16:27 +0100

    script

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,10 +1,3 @@
-import { fetchProducts, fetchUsers } from './api/api.js';
-import {
-	getDataFromLocalStorage,
-	saveDataToLocalStorage,
-} from './data/localStorage.js';
-import { highlightActiveBurgerLink } from './utils/utils.js';
-// import { renderHamburgerMenu, openNavMenu } from './components/navMenu.js';
 import { renderHamburgerMenu } from './components/navMenu.js';
 import { runIndexPage } from './pages/indexPage.js';
 import { runAboutPage } from './pages/aboutPage.js';
@@ -24,71 +17,108 @@ import { checkUserStatus } from './components/validateUser.js';
 console.log('Script.js loaded');
 
 const path = window.location.pathname;
-
-if (path === '/pages/' || path === '/pages/index.html') {
+// runIndexPage();
+if (
+	path === '/' ||
+	path === '/index.html' ||
+	path === '/yum-yum-gimme-sum/' ||
+	window.location.pathname === '/yum-yum-gimme-sum/index.html'
+) {
 	runIndexPage();
 	console.log('index.html');
-} else if (path === '/pages/about.html') {
+} else if (
+	path === '/about.html' ||
+	window.location.pathname === '/yum-yum-gimme-sum/about.html'
+) {
 	checkUserStatus();
 	runAboutPage();
 	renderHamburgerMenu();
 	openDropDownBasket();
 	console.log('about.html');
-} else if (path === '/pages/eta.html') {
+} else if (
+	path === '/eta.html' ||
+	window.location.pathname === '/yum-yum-gimme-sum/eta.html'
+) {
 	checkUserStatus();
 	runEtaPage();
 	renderHamburgerMenu();
 	openDropDownBasket();
 	console.log('eta.html');
-} else if (path === '/pages/history.html') {
+} else if (
+	path === '/history.html' ||
+	window.location.pathname === '/yum-yum-gimme-sum/history.html'
+) {
 	checkUserStatus();
 	runHistoryPage();
 	renderHamburgerMenu();
 	openDropDownBasket();
 	console.log('history.html');
-} else if (path === '/pages/map.html') {
+} else if (
+	path === '/map.html' ||
+	window.location.pathname === '/yum-yum-gimme-sum/map.html'
+) {
 	checkUserStatus();
 	runMapPage();
 	renderHamburgerMenu();
 	openDropDownBasket();
 	console.log('map.html');
-} else if (path === '/pages/menu.html') {
+} else if (
+	path === '/menu.html' ||
+	window.location.pathname === '/yum-yum-gimme-sum/menu.html'
+) {
 	checkUserStatus();
 	runMenuPage();
 	renderHamburgerMenu();
 	openDropDownBasket();
 	console.log('menu.html');
-} else if (path === '/pages/order-overview.html') {
+} else if (
+	path === '/order-overview.html' ||
+	window.location.pathname === '/yum-yum-gimme-sum/order-overview.html'
+) {
 	checkUserStatus();
 	runOrderOverviewPage();
 	renderHamburgerMenu();
 	console.log('order-overview.html');
-} else if (path === '/pages/orders.html') {
+} else if (
+	path === '/orders.html' ||
+	window.location.pathname === '/yum-yum-gimme-sum/orders.html'
+) {
 	checkUserStatus();
 	runOrdersPage();
 	renderHamburgerMenu();
 	openDropDownBasket();
 	console.log('orders.html');
-} else if (path === '/pages/profile.html') {
+} else if (
+	path === '/profile.html' ||
+	window.location.pathname === '/yum-yum-gimme-sum/profile.html'
+) {
 	checkUserStatus();
 	runProfilePage();
 	renderHamburgerMenu();
 	openDropDownBasket();
 	console.log('profile.html');
-} else if (path === '/pages/receipt.html') {
+} else if (
+	path === '/receipt.html' ||
+	window.location.pathname === '/yum-yum-gimme-sum/receipt.html'
+) {
 	checkUserStatus();
 	runReceiptPage();
 	renderHamburgerMenu();
 	openDropDownBasket();
 	console.log('receipt.html');
-} else if (path === '/pages/register.html') {
-	runRegisterPage();
+} else if (
+	path === '/register.html' ||
+	window.location.pathname === '/yum-yum-gimme-sum/register.html'
+) {
 	console.log('register.html');
-} else if (path === '/pages/editMenu.html') {
+
+	runRegisterPage();
+} else if (
+	path === '/editMenu.html' ||
+	window.location.pathname === '/yum-yum-gimme-sum/editMenu.html'
+) {
 	checkUserStatus();
 	runEditMenuPage();
 	renderHamburgerMenu();
 	console.log('editMenu.html');
 }
-
-// calls function that navigates you to menu.html when clicking on "Logga inn" in index page
